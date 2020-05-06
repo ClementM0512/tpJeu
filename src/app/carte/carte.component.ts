759d7b394372fa12068dcd2972ce2a5cdb067737
clem <clem.molinie3742@gmail.com>
2020-05-06 17:15:30 +0200

    final push for tp

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-carte',
@@ -8,17 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class CarteComponent implements OnInit {
 
   constructor() { }
-
+  @ViewChild("elem1", {read: ElementRef}) elem1: ElementRef;
   ngOnInit(): void {
   }
   fondCarte="../assets/imgs/cartes/fondcarte.png";
-  etatsCartes=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; 
-  nbPairesTrouvees=0;
   nbChoix=0;
   ids={'id1':0, 'id2':0};
   ctrlCarte(id){
    if (this.nbChoix==1) {
     this.ids.id2=id;
+    // console.log(this.elem1.nativeElement.childNodes[0].src);
+    
     this.controlePaire(this.ids);
     this.nbChoix =0;  
    }
